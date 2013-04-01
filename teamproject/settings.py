d056c57576b2f055191f265a99d1df83672da0f1
Chester Wells <cwells@jomc7896.dhcp.unc.edu>
2013-04-01 14:05:07 -0400

    commit

--- teamproject/settings.py
+++ teamproject/settings.py
@@ -1,7 +1,7 @@
 
 # Django settings for newschool project.
 
-import os
+import os, dj_database_url
 
 PROJECT_ROOT = os.path.dirname(__file__)
 PROJECT_PATH = os.path.dirname(PROJECT_ROOT)
@@ -163,3 +163,9 @@ LOGGING = {
         },
     }
 }
+# Parse database configuration from $DATABASE_URL
+
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
